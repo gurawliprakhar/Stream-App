61f73915b38a067c2d2fd7abf3161d997fd46b88
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2025-01-18 19:40:00 +0530

    updated

--- stream-front-end/src/App.jsx
+++ stream-front-end/src/App.jsx
@@ -6,12 +6,19 @@ import VideoUpload from './components/VideoUpload'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [videoId, setVideoId] = useState('06d68d31-ec63-4af1-96e9-88f12a90b949')
 
   return (
     <>
       <div className='flex flex-col items-center space-y-9 justify-center py-9'>
         <h1 className="text-5xl font-extrabold text-gray-700 dark:text-gray-100">
            welcome to video streaming application </h1>
+
+      <div>
+          <h1 className='text-white'>Playing Video </h1>
+          <video src = {`http://localhost:8080/api/v1/videos/stream/${videoId}`}  controls ></video>
+        </div>
+
       <VideoUpload/>
       </div>
     </>
